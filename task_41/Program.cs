96a2b67d02b4d684b3e7e821f876f1340cf7a08c
Grigory <free_time_88@mail.ru>
2023-07-29 14:32:39 +0500

    task41

---
+++ task_41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+
+void InputArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(-100, 100); 
+}
+
+int ReleaseArray(int[] array)
+{
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+if (array[i] > 0)
+count++;
+return count;
+
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(ReleaseArray(array));
